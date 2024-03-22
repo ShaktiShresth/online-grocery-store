5e8aa9787bebacac69fcaafb443ce5437fe168c3
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-03-22 14:00:42 +0545

    add slider section

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <main className="">Home</main>;
+import Slider from "./_components/Slider";
+import GlobalApi from "./_utils/GlobalApi";
+
+export default async function Home() {
+  const sliderList = await GlobalApi.getSliders();
+
+  return (
+    <main className="p-5 md:p-10 px-16">
+      {/* Slider */}
+      <Slider sliderList={sliderList} />
+    </main>
+  );
 }
